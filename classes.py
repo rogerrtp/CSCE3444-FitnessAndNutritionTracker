531baabb059f3c42a275dc98d3591862695ea003
Roger Roden <rogerr@trueprodigy.tech>
2025-10-14 20:05:13 -0500

    Added initial classes

---
+++ classes.py
@@ -0,0 +1,55 @@
+from sqlalchemy.orm import (
+    DeclarativeBase,
+    Mapped,
+    mapped_column,
+    relationship,
+    declared_attr
+)
+from sqlalchemy import (
+ForeignKey,
+String,
+Integer,
+DateTime,
+func
+)
+from typing import List, Optional
+from datetime import datetime
+
+
+class Base(DeclarativeBase):
+    pass
+
+class User(Base):
+    __tablename__ = "users"
+
+    userID: Mapped[int] = mapped_column(primary_key=True)
+    username: Mapped[str] = mapped_column(String(255))
+    firstName: Mapped[str] = mapped_column(String(255))
+    lastName: Mapped[str] = mapped_column(String(255))
+    password: Mapped[str] = mapped_column(String(255))  # TODO: hash password
+    is_active: Mapped[bool] = mapped_column(default=True)
+    createDt: Mapped[datetime] = mapped_column(DateTime, insert_default=func.now())
+    lastLoginDt: Mapped[datetime] = mapped_column(DateTime, insert_default=func.now())
+
+    def __repr__(self):
+        return f"<User(userID={self.userID}, username={self.name}, email={self.email})>"
+
+
+class LogEntryBase:
+    createDt: Mapped[datetime] = mapped_column(DateTime, insert_default=func.now())
+    entryDt: Mapped[datetime] = mapped_column(DateTime)
+    modifiedDt: Mapped[datetime] = mapped_column(DateTime)
+    userID: Mapped[int] = mapped_column(ForeignKey("users.userID"))
+
+    @declared_attr
+    def user(cls) -> Mapped["User"]:
+        return relationship("User")
+
+
+class MealLogEntry(LogEntryBase, Base):
+    __tablename__ = "meal_log_entries"
+    mealLogID: Mapped[int] = mapped_column(primary_key=True)
+    calories: Mapped[int] = mapped_column(String(255))
+    proteinGrams: Mapped[int] = mapped_column(Integer)
+    carbsGrams: Mapped[int] = mapped_column(Integer)
+    fatsGrams: Mapped[int] = mapped_column(Integer)
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from csce3444-fitnessandnutritiontracker!")
-
-
-if __name__ == "__main__":
-    main()
